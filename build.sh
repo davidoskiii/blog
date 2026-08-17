c5be9e92243412c8777b242765247b5a6c9dc945
davidoskiii <davide.mascherin9@gmail.com>
2026-08-17 20:23:14 +0200

    Added superuser to build

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py createsuperuser --noinput --username davide --email davide.mascherin9@gmail.com --password Davide070810? && python manage.py collectstatic --noinput
