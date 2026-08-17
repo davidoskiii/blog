8d5611525f770c982b11aba99f37c67c95a71b69
davidoskiii <davide.mascherin9@gmail.com>
2026-08-17 20:40:51 +0200

    Another try at fixing superuser at launch

--- build.sh
+++ build.sh
@@ -6,4 +6,6 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # Automate superuser creation (ignores duplicate errors if user exists)
-python manage.py createsuperuser --noinput || true
+python manage.py createsuperuser --noinput \
+  --username "${DJANGO_SUPERUSER_USERNAME:-admin}" \
+  --email "${DJANGO_SUPERUSER_EMAIL:-admin@example.com}" || true
